88f013588e668c112c12ddc9419129291e152fa0
Franky162 <116598490+Franky162@users.noreply.github.com>
2022-10-27 13:25:28 +0200

    test

---
+++ MyCompanyClient/MyCompanyClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCompanyClient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MyCompanyClient/MyCompanyClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCompanyClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
